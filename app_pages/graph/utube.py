8a45e02d420837cc5779681d22344090e1fd65ae
Aryan mallick <161678352+aryanma11ick@users.noreply.github.com>
2025-03-08 14:19:52 +0530

    Grok Initialization + link extraction

--- app_pages/graph/utube.py
+++ app_pages/graph/utube.py
@@ -27,3 +27,21 @@ from moviepy.editor import VideoFileClip
 import yt_dlp
 import os
 import openai
+import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+deepgram_api_key = os.getenv("DEEPGRAM_API_KEY")
+
+#Initializing Groq Client
+client1 = gt(api_key=groq_api_key)
+
+# Function to extract video ID from YouTube URL
+def extract_video_id(url):
+    match = re.search(r"v=([a-zA-Z0-9_-]+)", url)
+    if match:
+        return match.group(1)
+    else:
+        raise ValueError("Invalid YouTube URL: No video ID found")
+
